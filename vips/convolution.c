97222d0744021d686d4a96b5517df4c62c65590f
Frido Koch <info@fridokoch.de>
2021-03-17 22:41:44 +0100

    fix: Remove sharpen mode pt. III

--- vips/convolution.c
+++ vips/convolution.c
@@ -5,5 +5,5 @@ int gaussian_blur_image(VipsImage *in, VipsImage **out, double sigma, int precis
 }
 
 int sharpen_image(VipsImage *in, VipsImage **out, double sigma, double x1, double m2) {
-	return vips_sharpen(in, out, "sigma", sigma, "x1", x1, "m2", m2, "mode", mode, NULL);
+	return vips_sharpen(in, out, "sigma", sigma, "x1", x1, "m2", m2, NULL);
 }
